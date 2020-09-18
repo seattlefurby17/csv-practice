b86992a014b4bf5794874591ab81c3b2ee38031e
Hanh Solo <minhhanhnyc@gmail.com>
2020-09-18 10:31:11 -0700

    Tests are not passed but I have spent more than the recommended time box time

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -1,9 +1,31 @@
 require 'csv'
 require 'awesome_print'
 
-def get_all_olympic_athletes(filename)
+REQUIRED_OLYMPIAN_FIELDS = %w[ID Name Height Team Year City Sport Event Medal]
+MEDAL_TOTALS_FILENAME = 'data/medal_totals.csv'
+OLYMPIC_DATA_FILENAME = 'data/athlete_events.csv'
 
+def get_all_olympic_athletes(filename)
+  olympic_athletes = CSV.read(filename, headers: true).map(&:to_h)
+
+  olympic_athletes.each do |athlete|
+    array_of_field = []
+
+    athlete.select do |field|
+     athlete[:field] == REQUIRED_OLYMPIAN_FIELDS
+     array_of_field << athlete[:field]
+    end
+  end
+
+
+    # ap olympic_athletes
+  return olympic_athletes
 end
+
+ap get_all_olympic_athletes(OLYMPIC_DATA_FILENAME)
+
+# from_file = { a: 2, b: 3, c: 6 }
+# transofrmed = { a: from_file[:a], c: from_file[:c] }
 
 def total_medals_per_team(olympic_data)
 
